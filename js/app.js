1efebf16c94b7ab1a2e7aa29ad536e3c2cf895eb
danivent23122020 <dani.vent@orange.fr>
2022-09-29 11:10:15 +0200

    commit

--- js/app.js
+++ js/app.js
@@ -2,8 +2,18 @@
 const btnMenu = document.querySelector(".btn-rond-menu");
 const nav = document.querySelector(".nav-left");
 const ligne = document.querySelector(".cont-ligne");
-const allItemNav = document.querySelector(".nav-menu-item");
+const allItemNav = document.querySelectorAll(".nav-menu-item");
 //
 btnMenu.addEventListener("click", () => {
     ligne.classList.toggle("active");
+    nav.classList.toggle("menu-visible");
 });
+// media query for btn menu
+if (window.matchMedia("(max-width: 1200px)")) {
+    allItemNav.forEach((element) => {
+        element.addEventListener("click", () => {
+            nav.classList.toggle("menu-visible");
+            ligne.classList.toggle("active");
+        });
+    });
+}
