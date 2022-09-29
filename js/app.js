7202ce7e064e10b5aeaf2dde0bf4dd25ba6932a6
danivent23122020 <dani.vent@orange.fr>
2022-09-29 09:49:41 +0200

    commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-// alert('toto')
+// anim btn menu
+const btnMenu = document.querySelector(".btn-rond-menu");
+const nav = document.querySelector(".nav-left");
+const ligne = document.querySelector(".cont-ligne");
+const allItemNav = document.querySelector(".nav-menu-item");
+//
+btnMenu.addEventListener("click", () => {
+    ligne.classList.toggle("active");
+});
