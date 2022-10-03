227404c8bc034cbaf49f63dbc93d8c3557c0358e
danivent23122020 <dani.vent@orange.fr>
2022-10-03 21:24:44 +0200

    commit

--- js/app.js
+++ js/app.js
@@ -17,6 +17,23 @@ if (window.matchMedia("(max-width: 1200px)")) {
         });
     });
 }
+
+// *** scroll to top ***
+//display btn
+window.addEventListener("scroll", function () {
+    let scroll = this.document.querySelector(".btn-scrollup");
+    scroll.classList.toggle("active", window.scrollY > 200);
+});
+// scroll to top
+const btnScrollup = document.querySelector(".btn-scrollup");
+btnScrollup.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    });
+});
+
 // *** anim name type writer ****
 let nameAnim = document.querySelector(".name-animation");
 
@@ -114,7 +131,7 @@ window.addEventListener("load", () => {
 // *** anim ScrollMagic ****
 // anime presentation
 const presentationContainer = document.querySelector(".presentation");
-const titrePres = document.querySelector(".size-title");
+const titrePres = document.querySelector(".title-pres");
 const presGauche = document.querySelector(".pres-gauche");
 const listePres = document.querySelectorAll(".item-liste");
 // init timeline
@@ -304,3 +321,57 @@ const sceneComp = new ScrollMagic.Scene({
     .setTween(tlCompetences)
     // .addIndicators()
     .addTo(controller);
+
+// *** COOKIE modal ***
+let cookieModal = document.querySelector("#cookieModal");
+let acceptCookie = document.querySelector("#acceptCookie");
+let cancelCookie = document.querySelector("#refusedCookie");
+//
+cancelCookie.addEventListener("click", function () {
+    cookieModal.classList.remove("active");
+});
+//
+acceptCookie.addEventListener("click", function () {
+    let d = new Date();
+    d.setMinutes(2 + d.getMinutes());
+    document.cookie = "Barbershop=true; expires = ' + d + ';";
+    cookieModal.classList.remove("active");
+});
+
+// check cookie exist
+const checkCookie = () => {
+    let input = document.cookie.split("=");
+    if (input[0] == "Barbershop") {
+        cookieModal.classList.remove("active");
+    } else {
+        cookieModal.classList.add("active");
+    }
+};
+// check cookie exist page already load
+window.onload = () => {
+    setTimeout(() => {
+        checkCookie();
+    }, 1000);
+};
+//
+// test date
+let dateJour = Date.now();
+console.log(dateJour);
+console.log(dateJour / (1000 * 3600 * 24));
+
+function diffJoursDates(firstdate, seconddate) {
+    return (seconddate - firstdate) / (1000 * 3600 * 24);
+}
+let numDiff = diffJoursDates(new Date(2022, 09, 01), new Date(2022, 09, 27));
+console.log(numDiff);
+
+//
+const ladate = new Date();
+console.log("Nous sommes le : ");
+console.log(
+    ladate.getDate() +
+        "/" +
+        (ladate.getMonth() + 1) +
+        "/" +
+        ladate.getFullYear()
+);
